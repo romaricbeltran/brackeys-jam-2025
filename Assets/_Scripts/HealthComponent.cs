3abe215c59495a33d11747ec46c893603f2e0c95
GinoV <ginovalerio123@gmail.com>
2025-02-19 20:31:13 +0700

    created HealthComponents.cs

---
+++ Assets/_Scripts/HealthComponent.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public static Action<HealthComponent> OnHealthChanged;
+    public bool IsDead => _health < 0;
+
+    [SerializeField] private int _health = 100;
+
+    // value may be negative or positive
+    public void ChangeHealth(int value)
+    {
+        _health += value;
+
+        OnHealthChanged?.Invoke(this);
+    }
+}
