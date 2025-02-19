ac64ed54a98a1be48f1fecd0fe07f63b1640eb72
GinoV <ginovalerio123@gmail.com>
2025-02-20 02:57:41 +0700

    modified Shooter.cs - triggerable coroutine for shooting

--- Assets/_Scripts/Entities/Shooter.cs
+++ Assets/_Scripts/Entities/Shooter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
@@ -5,11 +6,54 @@ public class Shooter : MonoBehaviour
     [SerializeField] private Projectile _projectilePrefab;
     [SerializeField] private float _shootingInterval = 4f;
 
-    void Start()
+    private Coroutine m_corRef;
+
+    private bool isStop;
+
+    private void Start()
     {
-        InvokeRepeating("SpawnProjectile", _shootingInterval, _shootingInterval);
+        TriggerShootingCor();
     }
-    
+
+    public void TriggerShootingCor()
+    {
+        if (m_corRef == null)
+        {
+            m_corRef = StartCoroutine(ShootingCor());
+        }
+    }
+
+    private IEnumerator ShootingCor()
+    {
+        while (!isStop)
+        {
+            yield return new WaitForSeconds(_shootingInterval);
+            SpawnProjectile();
+        }
+
+        isStop =false;
+        m_corRef = null;
+    }
+
+    private void OnEnable()
+    {
+        HealthComponent.OnHealthChanged -= StopShooting;
+        HealthComponent.OnHealthChanged += StopShooting;
+    }
+
+    private void OnDisable()
+    {
+        HealthComponent.OnHealthChanged -= StopShooting;
+    }
+
+    private void StopShooting(HealthComponent healthComponent)
+    {
+        if (healthComponent.gameObject.tag == "Carrier" && healthComponent.IsDead)
+        {
+            isStop = true;
+        }
+    }
+
     private void SpawnProjectile()
     {
         Projectile spawnedProjectile = Instantiate(_projectilePrefab, GameObject.FindGameObjectWithTag("World").transform, true);
