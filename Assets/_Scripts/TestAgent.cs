f0affb3d09fed9d9fde48260b99e107be65053de
GinoV <ginovalerio123@gmail.com>
2025-02-19 13:18:56 +0700

    modified TestAgent.cs - controlling its speed

--- Assets/_Scripts/TestAgent.cs
+++ Assets/_Scripts/TestAgent.cs
@@ -4,28 +4,22 @@ using UnityEngine.AI;
 public class TestAgent : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float speed = 1f;
     [SerializeField] private float distanceThreshold = 1f;
 
     private NavMeshAgent navMeshAgent;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        navMeshAgent.speed = speed;
         navMeshAgent.stoppingDistance = distanceThreshold;
         navMeshAgent.SetDestination(target.position);
-        // if ((target.position - transform.position).magnitude >= distanceThreshold)
-        // {
-        //     navMeshAgent.SetDestination(target.position);
-        // }
-        // else
-        // {
-        //     navMeshAgent.stop
-        // }
     }
 }
