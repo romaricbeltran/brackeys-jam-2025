ba593968011aa81986e81123281c6425aad59a57
GinoV <ginovalerio123@gmail.com>
2025-02-20 03:04:48 +0700

    modified TransformDirection.cs - can set autoPilot mode to constantly adjust trajectory towards player

--- Assets/_Scripts/TransformModifiers/TransformDirection.cs
+++ Assets/_Scripts/TransformModifiers/TransformDirection.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class TransformDirection : MonoBehaviour
 {
+    [SerializeField] private bool _isAutoPilot;
+
     private Transform _target;
 
     public Transform Target
@@ -11,6 +13,19 @@ public class TransformDirection : MonoBehaviour
     }
 
     private void Start()
+    {
+        RotateTowardsTarget();
+    }
+
+    private void Update()
+    {
+        if(_isAutoPilot)
+        {
+            RotateTowardsTarget();
+        }
+    }
+
+    private void RotateTowardsTarget()
     {
         float angle = Mathf.Atan2(Target.position.y - transform.position.y, Target.position.x - transform.position.x) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
