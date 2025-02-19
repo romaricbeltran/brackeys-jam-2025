78d77f95c1f5ddeff6f9227f574053f217893f33
GinoV <ginovalerio123@gmail.com>
2025-02-20 01:55:34 +0700

    created TriggerEffect & TriggerHandler.cs

---
+++ Assets/_Scripts/Interactions/TriggerEffect.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[RequireComponent(typeof(TriggerHandler))]
+[RequireComponent(typeof(Lever))]
+public class TriggerEffect : MonoBehaviour
+{
+    private TriggerHandler m_triggerHandler;
+    private Lever m_lever;
+    private bool m_isTriggerState;
+
+    void Awake()
+    {
+        m_triggerHandler = GetComponent<TriggerHandler>();
+        m_lever = GetComponent<Lever>();
+    }
+
+    private void OnEnable()
+    {
+        m_triggerHandler.OnTriggerHappened -= SetTriggerState;
+        m_triggerHandler.OnTriggerHappened += SetTriggerState;
+
+        TestController.OnInteractRequest -= OnInteractRequest;
+        TestController.OnInteractRequest += OnInteractRequest;
+    }
+
+    private void OnDisable()
+    {
+        TestController.OnInteractRequest -= OnInteractRequest;
+        m_triggerHandler.OnTriggerHappened -= SetTriggerState;
+    }
+
+    private void SetTriggerState(OnTriggerPayload onTriggerPayload)
+    {
+        Debug.Log($"ExecuteTriggerEffect", this);
+        m_isTriggerState = onTriggerPayload.IsTriggerState;
+    }
+
+    private void OnInteractRequest(bool isInteractRequested)
+    {
+        if (isInteractRequested && m_isTriggerState)
+        {
+            m_lever.ToggleDoors();
+        }
+    }
+}
