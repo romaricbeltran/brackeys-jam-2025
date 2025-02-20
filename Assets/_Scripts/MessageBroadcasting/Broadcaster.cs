7896bbe4fe2abf2d90979c7e6177dab72dfabd4c
GinoV <ginovalerio123@gmail.com>
2025-02-20 16:36:45 +0700

    modified Broadcaster - added OnPlayerAnimationRequest event

--- Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
+++ Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
@@ -3,9 +3,15 @@ using System;
 public static class Broadcaster
 {
     public static event Action<AudioClipType> OnAudioRequest;
+    public static event Action<AnimationType> OnPlayerAnimationRequest;
 
     public static void TriggerOnAudioRequest(AudioClipType audioClipType)
     {
         OnAudioRequest?.Invoke(audioClipType);
+    }
+
+    public static void TriggerOnAnimationRequest(AnimationType animationType)
+    {
+        OnPlayerAnimationRequest?.Invoke(animationType);
     }
 }
