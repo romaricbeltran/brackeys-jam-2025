c6110036c7c7a5212030edfa934434e0c9be641a
GinoV <ginovalerio123@gmail.com>
2025-02-21 00:01:00 +0700

    modified Broadcaster.cs - added OnGameOver event

--- Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
+++ Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public static class Broadcaster
 {
     public static event Action<AudioClipType> OnAudioRequest;
     public static event Action<Transform, AnimationType> OnPlayerAnimationRequest;
+    public static event Action<GameOverPayLoad> OnGameOver;
 
     public static void TriggerOnAudioRequest(AudioClipType audioClipType)
     {
@@ -16,4 +16,14 @@ public static class Broadcaster
     {
         OnPlayerAnimationRequest?.Invoke(sender, animationType);
     }
+
+    public static void TriggerGameOver(GameOverPayLoad gameOverPayLoad)
+    {
+        OnGameOver?.Invoke(gameOverPayLoad);
+    }
+}
+
+public struct GameOverPayLoad
+{
+
 }
