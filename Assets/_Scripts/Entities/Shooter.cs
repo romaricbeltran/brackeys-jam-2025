1fba73c4178cbda45601d97cfd6c177cf0d8303a
Romaric BELTRAN <romaric.beltran@gmail.com>
2025-02-20 18:08:37 +0100

    Refacto shooter and cache player and carrier references

--- Assets/_Scripts/Entities/Shooter.cs
+++ Assets/_Scripts/Entities/Shooter.cs
@@ -7,12 +7,23 @@ public class Shooter : MonoBehaviour
     [SerializeField] private float _shootingInterval = 4f;
 
     private Coroutine m_corRef;
-
     private bool isStop;
+    private Transform m_player;
+    private Transform m_carrier;
 
     private void Start()
     {
+        FindStaticReferences();
         TriggerShootingCor();
+    }
+
+    private void FindStaticReferences()
+    {
+        GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO != null) m_player = playerGO.transform;
+
+        GameObject carrierGO = GameObject.FindGameObjectWithTag("Carrier");
+        if (carrierGO != null) m_carrier = carrierGO.transform;
     }
 
     public void TriggerShootingCor()
@@ -31,13 +42,11 @@ public class Shooter : MonoBehaviour
             SpawnProjectile();
         }
 
-        isStop =false;
         m_corRef = null;
     }
 
     private void OnEnable()
     {
-        HealthComponent.OnHealthChanged -= StopShooting;
         HealthComponent.OnHealthChanged += StopShooting;
     }
 
@@ -48,23 +57,21 @@ public class Shooter : MonoBehaviour
 
     private void StopShooting(HealthComponent healthComponent)
     {
-        if (healthComponent.gameObject.tag == "Carrier" && healthComponent.IsDead)
+        if (healthComponent.gameObject.CompareTag("Carrier") && healthComponent.IsDead)
         {
             isStop = true;
+            m_carrier = null;
         }
     }
 
     private void SpawnProjectile()
     {
-        Projectile spawnedProjectile = Instantiate(_projectilePrefab, GameObject.FindGameObjectWithTag("World").transform, true);
-        spawnedProjectile.transform.position = transform.position;
+        Projectile spawnedProjectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
 
-        var tempGOTarget = GameObject.FindGameObjectWithTag("Carrier").transform;
-        if(tempGOTarget == null)
+        Transform target = m_carrier ?? m_player;
+        if (target != null)
         {
-            tempGOTarget = GameObject.FindGameObjectWithTag("Player").transform;
+            spawnedProjectile.Init(target);
         }
-
-        spawnedProjectile.Init(tempGOTarget);
     }
 }
