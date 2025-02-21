7c9f75bf9d86c4423eb567e3d012d8c6704c557e
GinoV <ginovalerio123@gmail.com>
2025-02-22 00:53:18 +0700

    modified Broadcaster.cs - added  OnGameStart, OnPauseRequest, OnBackToMainPanel events

--- Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
+++ Assets/_Scripts/MessageBroadcasting/Broadcaster.cs
@@ -5,7 +5,11 @@ public static class Broadcaster
 {
     public static event Action<AudioClipType> OnAudioRequest;
     public static event Action<Transform, AnimationType> OnPlayerAnimationRequest;
+
     public static event Action<GameOverPayLoad> OnGameOver;
+    public static event Action<GameOverPayLoad> OnGameStart;
+    public static event Action<bool> OnPauseRequest;
+    public static event Action OnBackToMainPanel;
 
     public static void TriggerOnAudioRequest(AudioClipType audioClipType)
     {
@@ -20,6 +24,21 @@ public static class Broadcaster
     public static void TriggerGameOver(GameOverPayLoad gameOverPayLoad)
     {
         OnGameOver?.Invoke(gameOverPayLoad);
+    }
+
+    public static void TriggerOnPauseRequest(bool isActive)
+    {
+        OnPauseRequest?.Invoke(isActive);
+    }
+
+    public static void TriggerOnGameStart(GameOverPayLoad gameOverPayLoad)
+    {
+        OnGameStart?.Invoke(gameOverPayLoad);
+    }
+
+    public static void TriggerOnBackToMainPanel()
+    {
+        OnBackToMainPanel?.Invoke();
     }
 }
 
