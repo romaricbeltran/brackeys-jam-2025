b9c90f9e55f9191c1ebe0343a1e5046dca55c7da
QUIXINE <101377011+QUIXINE@users.noreply.github.com>
2025-02-23 00:54:42 +0900

    Finish Flower Pickup

--- Assets/Scripts/FlowerPotion.cs
+++ Assets/Scripts/FlowerPotion.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class FlowerPotion : MonoBehaviour
 {
+
+    #region Gino Code
+
     public void PickUpFLower()
     {
         var tempCarrierRef = GameObject.FindGameObjectWithTag("Carrier");
@@ -17,4 +20,29 @@ public class FlowerPotion : MonoBehaviour
             }
         }
     }
+
+    #endregion
+    
+   
+    [SerializeField] HealthComponent _healthComponent;
+    private bool _playerCollided;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !_playerCollided)
+        {
+            //put flower to the hold position
+            //child to player
+            GameObject flowerHolder = GameObject.FindWithTag("FlowerHolder");
+            transform.parent = other.transform;
+            transform.position = flowerHolder.transform.position;
+            _playerCollided = true;
+        }
+        else if (other.gameObject.CompareTag("Carrier") && _playerCollided)
+        {
+            _healthComponent.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+
 }
