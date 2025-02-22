4f6099e5da9e8a53b8b10215f2ca158815439322
QUIXINE <101377011+QUIXINE@users.noreply.github.com>
2025-02-23 01:36:50 +0900

    Add FlowerHolder to Player prefab and edit FlowerPotion.cs

--- Assets/Scripts/FlowerPotion.cs
+++ Assets/Scripts/FlowerPotion.cs
@@ -7,29 +7,25 @@ public class FlowerPotion : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player") && !_playerCollided)
+        if (other.gameObject.CompareTag("Carrier") && _playerCollided)
+        {
+            var carrierHealthComponent = other.GetComponent<HealthComponent>();
+
+            if(carrierHealthComponent != null)
+            {
+                carrierHealthComponent.ChangeHealth(+1);
+                Destroy(gameObject);
+            }
+        }
+        else if (other.gameObject.CompareTag("Player") && !_playerCollided)
         {
             //put flower to the hold position
             //child to player
             GameObject flowerHolder = GameObject.FindWithTag("FlowerHolder");
-            transform.parent = other.transform;
+            transform.parent = flowerHolder.transform;
             transform.position = flowerHolder.transform.position;
             _playerCollided = true;
-        }
-        else if (other.gameObject.CompareTag("Carrier") && _playerCollided)
-        {
-            var tempCarrierRef = GameObject.FindGameObjectWithTag("Carrier");
 
-            if(tempCarrierRef != null)
-            {
-                var carrierHealthComponent = tempCarrierRef.GetComponent<HealthComponent>();
-
-                if(carrierHealthComponent != null)
-                {
-                    carrierHealthComponent.ChangeHealth(+1);
-                    Destroy(gameObject);
-                }
-            }
         }
     }
 
